914beade735dc1bf382780967d9ef24b1bddb8f6
GaudinVincent <v.gaudin06@yahoo.fr>
2023-02-20 14:18:25 +0100

    modif nav

---
+++ src/assets/error/error.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Error() {
+  return (
+    <div>
+      <h1>La page n'a pas pu être chargée</h1>
+    </div>
+  );
+}
+export default Error;
--- src/navigation/nav.js
+++ src/navigation/nav.js
@@ -1,10 +1,24 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import CreateAccount from "../components/createAccount/createAccount";
 import Home from "../components/home/home";
+import PagePerso from "../components/pageperso/pageperso";
+import Error from "../assets/error/error";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/pageperso",
+    element: <PagePerso />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/createAccount",
+    element: <CreateAccount />,
+    errorElement: <Error />,
   },
 ]);
 
